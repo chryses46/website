a6f092fe88b9efcd9ea2e38b5f0ff38d37456b9f
Daniel Frank <danjfrank08@gmail.com>
2025-02-11 02:42:10 -0600

    cleaning up

--- app.js
+++ app.js
@@ -2,14 +2,11 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var morgan = require('morgan');
-const expressWinston = require('express-winston');
-const logger = require('utils/logger');
-require('dotenv').config({ path: '.env.development' });
+var logger = require('morgan');
+require('dotenv').config({ path: './.env.development' });
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const sampleRouter = require('./routes/sampleRoute');
 
 var app = express();
 
@@ -17,15 +14,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(morgan('combined'));
-app.use(expressWinston.logger({
-  winstonInstance: logger,
-  meta: true,
-  msg: "HTTP {{req.method}} {{req.url}}",
-  expressFormat: true,
-  colorize: false,
-  ignoreRoute: function (req, res) { return false; }
-}));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -33,25 +22,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/samples', sampleRouter); 
 
 // Catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  next(createError(404));
 });
 
-// Error handler
+// error handler
 app.use(function(err, req, res, next) {
-  // Set locals, only providing error in development
+  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // Log the error using Winston
-  logger.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
-
-  // Render the error page
+  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
